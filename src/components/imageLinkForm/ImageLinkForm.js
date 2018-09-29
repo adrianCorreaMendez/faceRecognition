87141418a84a500de5bec9cd972edf91ef800c74
adrianjcorrea <adrianjcorrea007@gmail.com>
2018-09-29 16:45:00 -0500

    added image link form

--- src/components/imageLinkForm/ImageLinkForm.js
+++ src/components/imageLinkForm/ImageLinkForm.js
@@ -1,12 +1,19 @@
 import React, {Component} from 'react';
+import './ImageLinkForm.css';
 
 const ImageLinkForm = () => {
   return (
     //adding div  styling
     <div>
-      <p className='ma4 mt0'>
-        {'This Artificial Intelligent Mind Will detect faces'}
+      <p className='f3'>
+        {'This Artificial Intelligent Mind Will detect faces in pictures'}
       </p>
+       <div className='center'>
+        <div className=' form pa4 br3 shadow-5'>
+          <input className='f4 pa2 w-70 center' type='tex'/>
+          <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+        </div>
+       </div>
     </div>
   );
 }
