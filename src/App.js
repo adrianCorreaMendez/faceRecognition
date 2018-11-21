10f5cff3b2018d3be6329c38444d64acce07130f
adrianjcorrea <adrianjcorrea007@gmail.com>
2018-11-21 13:20:59 -0600

    finished front end for faceRecognition

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Logo from './components/logo/Logo.js';
 import ImageLinkForm from './components/imageLinkForm/ImageLinkForm';
 import FaceRecognition from './components/faceRecognition/FaceRecognition.js';
 import Rank from './components/rank/Rank.js';
+import SignIn from './components/signIn/SignIn.js';
+import Register from './components/register/Register.js';
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 import './App.css';
@@ -30,7 +32,9 @@ class App extends Component {
     this.state = {
       input: '',
       imageUrl:'',
-      box: {}
+      box: {},
+      route: 'signin',
+      isSignedIn:false
     }
   }
 
@@ -64,21 +68,39 @@ class App extends Component {
         .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
         .catch(err => console.log('error'));
   }
+   onRouteChange = (route) => {
+     if(route === 'signout'){
+       this.setState({isSignedIn: false})
+     }else if (route === 'home'){
+       this.setState({isSignedIn: true})
+     }
+     this.setState({route: route})
+   }
 
   render() {
+    const {isSignedIn, box, imageUrl, route} = this.state;
     return (
       <div className="App">
       <Particles className='particles'
-           params={particlesOptions}
-           />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm
-         onInputChange={this.onInputChange}
-         onButtonSubmit ={this.onButtonSubmit}/>
-        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
-      </div>
+            params={particlesOptions}
+            />
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+        {route === 'home'
+        ? <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit ={this.onButtonSubmit}/>
+            <FaceRecognition box={box} imageUrl={imageUrl}/>
+          </div>
+          : (
+            route === 'signin'
+            ? <SignIn onRouteChange={this.onRouteChange} />
+            :<Register onRouteChange={this.onRouteChange} />
+          )
+        }
+        </div>
     );
   }
 }
--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
-const Navigation = () => {
-  return (
+const Navigation = ({onRouteChange,isSignedIn,signin}) => {
+    if(isSignedIn){
+      return (
     //adding nav styling
-    <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-    <p className='fs link dim black underline pa3 pointer'> SignOut </p>
-    </nav>
+      <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+        <p onClick={() => onRouteChange('signout')} className='fs link dim black underline pa3 pointer'> Sign out </p>
+      </nav>
+    );
+    }else{
+      return(
+      <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+        <p onClick={() => onRouteChange('signin')} className='fs link dim black underline pa3 pointer'> SignIn </p>
+        <p onClick={() => onRouteChange('Register')} className='fs link dim black underline pa3 pointer'> Register</p>
+      </nav>
   );
+ }
 }
 export default Navigation;
